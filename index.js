9530af3332c5cd21ad3daeafb1ea0fc8047bcf4b
ColinYergin <yergicol@umich.edu>
2017-05-05 00:32:30 -0400

    heroku try #3

--- index.js
+++ index.js
@@ -193,5 +193,5 @@ server.put('/', function(req, res) {
 });
 
 server.listen(server_port, /*server_ip_address,*/ function() {
-	console.log("Listening on " + server_ip_address + ", server_port " + server_port);
+	console.log("Listening on "/* + server_ip_address*/ + ", server_port " + server_port);
 });
