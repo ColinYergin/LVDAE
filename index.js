beb1514dd653c162e5f5367d63c93993de620977
ColinYergin <yergicol@umich.edu>
2017-05-04 21:53:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,195 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+
+var server = express();
+var port = 1001;
+
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+server.use(express.static(path.join(__dirname, '/static')))
+
+server.get('/', function(req, res) {
+	console.log(req.url);
+	res.sendFile('views/index.html', {root: __dirname});
+});
+
+Consonants = "ᑭ ᑲ ᕵ ᕹ ᒉ ᒍ ᖴ ᖶ ᕂ ᕆ ᖋ ᖍ ᒥ ᒪ ᕮ ᕭ ᕳ ᕲ ᗭ ᗪ".split(" ");
+Vowels = "ᐁ ᐍ ᐃ ᐏ ᐯ ᐻ ᐱ ᐽ ᑌ ᑘ ᑎ ᑚ".split(" ");
+SimpleVoicedConsonants = "ᑲ ᕹ ᒍ ᖶ ᕆ ᖍ ᒪ ᕭ ᕲ ᗪ".split(" ");
+SimpleUnvoicedConsonants = "ᑭ ᕵ ᒉ ᖴ ᕂ ᖋ ᒥ ᕮ ᕳ ᗭ".split(" ");
+TranslateSpaces = t => t.replace(/ /g, "　");
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if(this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+Color = (r,g,b)=>"rgb("+r*255+","+g*255+","+b*255+")";
+
+SecretKey = (new Date()).toUTCString().hashCode();
+
+function LevelParse(str) {
+	var grouplist = JSON.parse(str);
+	var levels = [];
+	for(var g = 0; g < grouplist.length; g += 1) {
+		var group = grouplist[g];
+		var gstartl = levels.length;
+		for(var l = 0; l < group.length; l += 1) {
+			var level = group[l];
+			for(var i = 0; i < otherwise(level.count, 1); i += 1) {
+				level.trappos = gstartl;
+				// Swap in the wider spaces
+				if(level.trap) 		level.trap 		= level.trap.map(TranslateSpaces);
+				if(level.answer) 	level.answer 	= TranslateSpaces(level.answer);
+				if(level.alt) 		level.alt 		= TranslateSpaces(level.alt);
+				if(level.prompt)	level.prompt 	= TranslateSpaces(level.prompt);
+				levels.push(level);
+			}
+		}
+	}
+	return levels;
+}
+var Levels = LevelParse(fs.readFileSync(path.join(__dirname, 'data/levels.json'), {encoding: 'utf8'}));
+
+function getRndInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+function randElt(arr) {
+	return arr[getRndInt(0, arr.length)];
+}
+function otherwise(primary, secondary) {
+	if(primary) return primary;
+	else return secondary;
+}
+
+function TemplateGenerator() {
+	var s = randElt(SimpleUnvoicedConsonants);
+	var m = Consonants[1 + Consonants.indexOf(s)];
+	var z = m;
+	while(z === m) {z = randElt(SimpleVoicedConsonants);}
+	var Z_ = randElt(SimpleVoicedConsonants);
+	var M_ = randElt(SimpleVoicedConsonants);
+	var cmds = {
+		C: () => randElt(Consonants),
+		S: () => {
+			var res = randElt(SimpleUnvoicedConsonants);
+			M_ = Consonants[1 + Consonants.indexOf(res)];
+			while(Z_ === M_) {Z_ = randElt(SimpleVoicedConsonants);}
+			return res;
+		},
+		Z: () => Z_,
+		M: () => M_,
+		V: () => randElt(Vowels),
+		s: () => s,
+		z: () => z,
+		m: () => m
+	};
+	return {
+		get: c => otherwise(cmds[c], () => c)()
+	};
+}
+
+function DoGen(genobj) {
+	if(String(genobj) === genobj) {
+		var tgen = TemplateGenerator();
+		var res = "";
+		for(var i = 0; i < genobj.length; i += 1) {
+			res = res + tgen.get(genobj.charAt(i));
+		}
+		return res;
+	} else if(Array.isArray(genobj)) {
+		return DoGen(randElt(genobj));
+	} else if(genobj.type === "or") {
+		var right = DoGen(genobj.right);
+		var wrong = DoGen(genobj.wrong);
+		var res = {
+			prompt: TranslateSpaces("ᕵᑌ " + randElt([right + " " + wrong, wrong + " " + right])),
+			answer: right.replace(/[\[\]]/g, ""),
+			trap: [wrong.replace(/[\[\]]/g, "")]
+		};
+		return res;
+	} else {
+		console.log("Unrecognized gen type ", genobj.type);
+	}
+}
+
+makeLevelRes = function(nextlevel) {
+	var levelres = Levels[nextlevel];
+	if(levelres.draw) {
+		var canvasw = 750;
+		var canvash = 400;
+		for(var i = 0; i < levelres.draw.length; i++) {
+			var step = levelres.draw[i];
+			if(step.loc === "center") {
+				step.loc = {x:canvasw/2,y:canvash/2};
+			} else if(step.loc === "random") {
+				var margin = 50;
+				step.loc = {x:getRndInt(margin,canvasw-margin),y:getRndInt(margin,canvash-margin)};
+			}
+			if(step.color === "random") {
+				step.color = Color(Math.random()*.5,Math.random()*.5,Math.random()*.5);
+			}
+		}
+	}
+	if(levelres.promptShuffle) {
+		levelres.prompt = levelres.promptShuffle.split(" ").sort((a, b)=>randElt([-1, 1])).join("　"); // joins on wide space
+	}
+	if(levelres.gen) {
+		var genRes = DoGen(levelres.gen)
+		levelres.prompt = genRes.prompt;
+		levelres.answer = genRes.answer;
+		levelres.trap = genRes.trap;
+		console.log("Generating from ", levelres.gen, " got ", genRes);
+	}
+	return {
+		prompt: otherwise(levelres.prompt, " "),
+		level: nextlevel,
+		prefix: levelres.prefix,
+		emphasis: levelres.emphasis,
+		limit: levelres.limit,
+		matchHash: levelres.answer.hashCode(), // For the client to know if they're right
+		checkHash: (levelres.answer + SecretKey).hashCode(), // For us to know they didn't cheat
+		trapMatchHashes: otherwise(levelres.trap, []).map(t => t.hashCode()),
+		trapCheckHashes: otherwise(levelres.trap, []).map(t => (t + SecretKey).hashCode()),
+		draw: levelres.draw
+	};
+}
+
+server.put('/', function(req, res) {
+	console.log(req.body);
+	if(req.body.cheat) {
+		res.json(makeLevelRes(req.body.cheat));
+	}
+	if(!(req.body.hasOwnProperty('guess') && req.body.hasOwnProperty('level'))) {
+		console.log('Badly formatted put request');
+		console.log(req.body);
+	} else {
+		var level = Levels[req.body.level];
+		if(level) {
+			if((req.body.guess + SecretKey).hashCode() === req.body.checkHash || level.autoPass === 1) {
+				res.json(makeLevelRes(req.body.level + 1));
+			} else {
+				var nextlevel;
+				if(req.body.trapCheckHashes.includes((req.body.guess + SecretKey).hashCode())) {
+					nextlevel = level.trappos;
+				}
+				if(nextlevel) {
+					res.json(makeLevelRes(nextlevel));
+				} else {
+					res.json({});
+				}
+			}
+		}
+	}
+});
+
+server.listen(port, function() {
+	console.log('server listening on port ' + port);
+});
---
+++ static/scripts.js
@@ -0,0 +1,210 @@
+var alphabet = "ᐁ	ᐍ	ᐃ	ᐏ	ᐯ	ᐻ	ᐱ	ᐽ	ᑌ	ᑘ	ᑎ	ᑚ	ᑭ	ᑲ	ᕵ	ᕹ	ᒉ	ᒍ	ᖴ	ᖶ	ᕂ	ᕆ	ᖋ	ᖍ	ᒥ	ᒪ	ᕮ	ᕭ	ᕳ	ᕲ	ᗭ	ᗪ	　".split("\t");
+var letterkey= "a	A	s	S	d	D	f	F	j	J	k	K	g	h	q	w	e	r	u	i	o	p	t	y	z	x	c	v	b	n	m	l	 ".split("\t");
+var IPAletter= "i	ɪ	u	ʊ	ɘ	eɪ	o	ʌ	ɛ	æ	a	ɑ	s	z	f	v	t	d	θ	ð	k	g	p	b	j	ŋ	ɹ	l	m	n	ʃ	ʒ	 ".split("\t");
+var level = 0;
+var limit = 0;
+var forcePrefix = "";
+var InputLocked = 0;
+var matchHash = "";
+var checkHash = "";
+var trapMatchHashes = [];
+var trapCheckHashes = [];
+
+cheat = (n) => DoSubmit({cheat:n});
+
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if(this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+back = (a) => a[a.length-1];
+function otherwise(primary, secondary) {
+	if(primary) return primary;
+	else return secondary;
+}
+
+Color = (r,g,b)=>"rgb("+r*255+","+g*255+","+b*255+")";
+Place = (x,y)=>({x:x,y:y});
+
+keyPressed = function(event) {
+	var input = document.getElementById("maininput");
+	console.log(event);
+	if(InputLocked === 1) {
+		event.preventDefault();
+	} else if(event.charCode > 0 && !event.ctrlKey) {
+		var pre = input.value.substr(0, input.selectionStart);
+		var post = input.value.substr(input.selectionEnd);
+		event.preventDefault();
+		pre = pre.concat(translateLetter(event.key))
+		input.value = pre.concat(post);
+		input.selectionStart = pre.length;
+		input.selectionEnd = pre.length;
+		enforcePrefix();
+		submit(input.value);
+	}
+}
+keyUp = function(event) {
+	var input = document.getElementById("maininput");
+	input.value = translateString(input.value);
+	if(event.key.length !== 1 && InputLocked == 0) {
+		enforcePrefix();
+		submit(input.value);
+	}
+}
+
+window.onload = function() {
+	submit('');
+}
+
+translateString = function(str) {
+	return str.replace(/./g, translateLetter);
+}
+translateLetter = function(c) {
+	var ind = letterkey.indexOf(c);
+	if(alphabet.indexOf(c) !== -1) {
+		return c;
+	} else if(ind === -1) {
+		return "";
+	} else {
+		return alphabet[ind];
+	}
+}
+enforcePrefix = function() {
+	var input = document.getElementById("maininput");
+	if(!input.value.startsWith(forcePrefix)) {
+		var startpos = input.selectionStart;
+		var endpos = input.selectionEnd;
+		input.value = forcePrefix.concat(input.value);
+		input.selectionStart = startpos + forcePrefix.length;
+		input.selectionEnd = endpos + forcePrefix.length;
+	}
+	if(input.value.length > limit && limit != 0) {
+		input.value = input.value.substr(0, limit)
+	}
+}
+
+submit = function(string) {
+	var hash = string.hashCode();
+	if(hash === matchHash || trapMatchHashes.includes(hash) || level === 0) {
+		DoSubmit({
+			guess: string,
+			level: level,
+			checkHash: checkHash,
+			trapCheckHashes: trapCheckHashes
+		});
+	}
+}
+
+function DoSubmit(obj) {
+	console.log("Doing submit");
+	xhr = new XMLHttpRequest();
+	xhr.open('PUT', '');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onload = function() {
+		if (xhr.status === 200) {
+			gotResponse(JSON.parse(xhr.responseText));
+		} else if (xhr.status !== 200) {
+			console.log('Request failed');
+		}
+	};
+	xhr.send(JSON.stringify(obj));
+}
+
+gotResponse = function(response) {
+	if(response.prompt) {
+		var input = document.getElementById("maininput");
+		var startNextLevel = function() {
+			level = response.level;
+			limit = response.limit;
+			if(response.prefix) {
+				forcePrefix = response.prefix;
+				input.value = forcePrefix;
+			} else {forcePrefix = "";}
+			if(response.emphasis) {
+				var color = "color:rgb(" + parseInt(130 * response.emphasis) + "," + parseInt(54 * response.emphasis) + "," + parseInt(155 * response.emphasis) + ");";
+				response.prompt = response.prompt.replace(/\[/g, "<font style=\"" + color + "\">").replace(/\]/g, "</font>");
+			}
+			document.getElementById("maininput").value = "";
+			var prompt = document.getElementById("prompt");
+			prompt.innerHTML = response.prompt;
+			prompt.title = "";
+			for(var i = 0; i < response.prompt.length; i += 1) {
+				var ind = alphabet.indexOf(response.prompt.charAt(i));
+				if(ind != -1) {
+					prompt.title += IPAletter[ind];
+				}
+			}
+			if(response.map) {console.log("Got map in response, not supported");}
+			matchHash = response.matchHash;
+			checkHash = response.checkHash;
+			trapMatchHashes = response.trapMatchHashes;
+			trapCheckHashes = response.trapCheckHashes;
+			clearDraw();
+			if(response.draw) {DrawSpec(response.draw);}
+		}
+		if(level > response.level) FlashInputColor("red", 750, startNextLevel);
+		else if(level <= response.level) FlashInputColor("green", 250, startNextLevel);
+	}
+}
+
+FlashInputColor = function(color, time, callback) {
+	var input = document.getElementById("maininput");
+	var old = input.style.backgroundColor;
+	input.style.backgroundColor = color;
+	InputLocked = 1;
+	setTimeout(function() {
+		if(input.style.backgroundColor === color)
+			input.style.backgroundColor = old;
+		InputLocked = 0;
+		callback();
+	}, time);
+}
+
+function DrawSpec(spec) {
+	var ctx = document.getElementById("draw").getContext("2d");
+	var pen = new Pen();
+	var cmds = {
+		rect: (p)=>pen.Rect(p.loc, p.w, p.h),
+		circle: (p)=>pen.Circle(p.loc, p.r),
+		polygon: (p)=>pen.Polygon(p.points),
+		move: (p)=>pen.Move(p.loc),
+		reset: (p)=>pen.Reset(),
+		color: (p)=>pen.color = p.color,
+	};
+	for(var i = 0; i < spec.length; ++i) {
+		if(spec[i]) {
+			console.log(pen.offset);
+			cmds[spec[i].op](spec[i]);
+		}
+	}
+}
+
+function Pen() {
+	this.color = Color(0,0,0);
+	this.offset = Place(0, 0);
+};
+Pen.prototype.Reset = function() {this.offset = Place(0, 0);};
+Pen.prototype.Draw = function(f) {draw(f, this.color)};
+Pen.prototype.Move = function(p) {this.offset.x += p.x; this.offset.y += p.y};
+Pen.prototype.Rect = function(loc,w,h) {this.Draw((ct) => ct.rect(loc.x-w/2+this.offset.x, loc.y-h/2+this.offset.y, w, h));};
+Pen.prototype.Circle = function(loc,r) {this.Draw((ct) => ct.arc(loc.x+this.offset.x, loc.y+this.offset.y, r, 0, 2*Math.PI));};
+Pen.prototype.Polygon = function(a) {this.Draw((ct) => {ct.moveTo(back(a).x+this.offset.x,back(a).y+this.offset.y);a.forEach((p)=>ct.lineTo(p.x+this.offset.x,p.y+this.offset.y))});};
+
+function draw(pathcb, color) {
+	var ctx = document.getElementById("draw").getContext("2d");
+	ctx.beginPath();
+	pathcb(ctx);
+	ctx.closePath();
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+function clearDraw() {
+	var canvas = document.getElementById("draw")
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
